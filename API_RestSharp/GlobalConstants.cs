c30490ecc6a7c7eb75ebbc3c87d9bf208a5f2259
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-12 12:34:54 +0100

    Create GlobalConstants.cs

---
+++ API_RestSharp/GlobalConstants.cs
@@ -0,0 +1,29 @@
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Net;
+
+namespace RecipeBook
+{
+    public static class GlobalConstants
+    {
+        public const string BaseUrl = "http://localhost:5000/api";
+
+        public static string AuthenticateUser(string email, string password)
+        {
+            var authClient = new RestClient(BaseUrl);
+            var request = new RestRequest("user/login", Method.Post);
+            request.AddJsonBody(new { email, password });
+
+            var response = authClient.Execute(request);
+
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                Assert.Fail($"Authentication failed with status code: {response.StatusCode}, " +
+                    $"content: {response.Content}");
+            }
+
+            var content = JObject.Parse(response.Content);
+            return content["token"]?.ToString();
+        }
+    }
+}
